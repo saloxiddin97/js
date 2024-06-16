86dae1ee6ef492aa014f88e894cf8ef45f073453
saloxiddin97 <salohiddinshamsidinov0101@gmail.com>
2024-06-16 10:02:40 +0500

    js9

---
+++ script.js
@@ -0,0 +1,20 @@
+let a=["salon", "saloxdin", "shamsuddiniv", "banan",]
+let s=["rot", "tor", "shamsudd", "bay",]
+
+//for(i=0;i<a.length; i++){
+   // blur.push(a[i].toUpperCase())
+//}
+//console.log(b);
+for(sozlar of a){
+    if(sozlar.toLowerCase().includes("n")){
+        console.log(sozlar);
+        console.log("n haripi ishtrok etgansozlar");  
+    }
+   
+}
+for(sozla of s){
+    if(sozla.toLowerCase()){
+        console.log(sozla);
+        console.log("n haripi ishtrok etmagansozlar");  
+    }
+}
